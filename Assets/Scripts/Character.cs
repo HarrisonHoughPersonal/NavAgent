96fbebd6285d2b6ff88855d6fefb8b6f03c184a3
Harrison Hough <harrisonhough4@gmail.com>
2018-08-29 00:55:27 +1000

    Basic setup

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Character : MonoBehaviour {
+
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private NavMeshAgent agent;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector3.Distance(transform.position, agent.pathEndPosition) > 0.1f)
+        {
+            anim.SetBool("InMotion", true);
+        }
+        else
+        {
+            anim.SetBool("InMotion", false);
+        }
+
+        /*if (agent.velocity.x != 0 || agent.velocity.y != 0)
+        {
+            anim.SetBool("InMotion", true);
+        }
+        else
+        {
+            anim.SetBool("InMotion", false);
+        }*/
+	}
+
+
+}
